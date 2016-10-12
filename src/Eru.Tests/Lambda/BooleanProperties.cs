61d4314bfb160e08ee3ac825c7d12f598879781c
Maurice CGP Peters <mcgppeters@hotmail.com>
2016-10-12 11:24:27 +0200

    Fix church True boolean value

--- src/Eru.Tests/Lambda/BooleanProperties.cs
+++ src/Eru.Tests/Lambda/BooleanProperties.cs
@@ -14,7 +14,7 @@ namespace Eru.Tests.Lambda
         [Property(Verbose = true)]
         public void True_returns_first_parameter_when_this_value_represents_the_boolean_value_of_true(int @true, object @false)
         {
-            Assert.Equal(@true, Bool.True1(@true, @false));
+            Assert.Equal(@true, Bool.True(@true)(@false));
         }
 
         [Property(Verbose = true)]
