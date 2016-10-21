e5d8c40313c2808633baa4b478b7dcde6d0f1e1c
Maurice CGP Peters <mcgppeters@hotmail.com>
2016-10-21 09:13:54 +0200

    Church booleans

--- src/Eru.Tests/Lambda/BooleanProperties.cs
+++ src/Eru.Tests/Lambda/BooleanProperties.cs
@@ -11,16 +11,19 @@ namespace Eru.Tests.Lambda
 {
     public class BooleanProperties
     {
+        public static readonly Eru.Lambda.Boolean True = Bool.True;
+        public static readonly Eru.Lambda.Boolean False = Bool.False;
+
         [Property(Verbose = true)]
-        public void True_returns_first_parameter_when_this_value_represents_the_boolean_value_of_true(int @true, object @false)
+        public void True_returns_first_parameter_when_this_value_represents_the_boolean_value_of_true(int @true, string @false)
         {
-            Assert.Equal(@true, Bool.True(@true)(@false));
+            Assert.Equal(@true, True(@true)(@false));
         }
 
         [Property(Verbose = true)]
         public void False_returns_the_second_parameter_when_this_value_represents_the_boolean_value_of_true(int @true, string @false)
         {
-            Assert.Equal(@false, Bool.False(@true)(@false));
+            Assert.Equal(@false, False(@true)(@false));
         }
     }
 }
--- src/Eru/Lambda/Boolean.cs
+++ src/Eru/Lambda/Boolean.cs
@@ -9,5 +9,15 @@ namespace Eru.Lambda
     {
         public static Boolean True => @true => @false => @true;
         public static Boolean False => @true => @false => @false;
+
+        public static bool ToBool(this Boolean @bool)
+        {
+            return (bool)@bool(true)(false);
+        }
+        //  User-defined conversion from a boolean value to a Boolean lambda
+        public static Boolean ToBoolean(this bool @bool)
+        {
+            return @bool ? True : False;
+        }
     }
 }
