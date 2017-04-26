c6fc8078be7df1bbc92d5c0773f1ba33a0bbc5e3
Maurice CGP Peters <mcgppeters@hotmail.com>
2017-04-26 19:52:18 +0200

    Adjustment to handle Either changes

--- src/Eru/Validation/Validation.cs
+++ src/Eru/Validation/Validation.cs
@@ -6,48 +6,48 @@
 
     public static class Validation
     {
-        private static Either<TRight, TCauseIdentifier[]> Assert<TRight, TCauseIdentifier>(TRight source,
-            params Property<TCauseIdentifier, TRight>[] properties)
+        private static Either<TSuccess, TCauseIdentifier> Assert<TSuccess, TCauseIdentifier>(TSuccess source,
+            params Property<TCauseIdentifier, TSuccess>[] properties)
         {
             var propertiesThatDoNotHold = properties
                 .Where(rule => !rule.Holds(source))
                 .Select(rule => rule.Identifier)
                 .ToArray();
 
-            return propertiesThatDoNotHold.Any()
-                ? source(propertiesThatDoNotHold)
-                : source.Return();
+            return propertiesThatDoNotHold.Any() 
+                ? propertiesThatDoNotHold.Fail<TSuccess, TCauseIdentifier>() 
+                : new Either<TSuccess, TCauseIdentifier>.Success(source);
         }
 
-        public static Either<TCauseIdentifier[], TRight> Check<TRight, TCauseIdentifier>(
-            this Either<TCauseIdentifier[], TRight> source,
+        public static Either<TRight, TCauseIdentifier> Check<TRight, TCauseIdentifier>(
+            this Either<TRight, TCauseIdentifier> source,
             params Property<TCauseIdentifier, TRight>[] properties)
         {
             return source.Bind(right => Assert(right, properties));
         }
 
-        public static Either<TCauseIdentifier[], TRight> Check<TRight, TCauseIdentifier>(this TRight source,
+        public static Either<TRight, TCauseIdentifier> Check<TRight, TCauseIdentifier>(this TRight source,
             params Property<TCauseIdentifier, TRight>[] properties)
         {
-            return Check(source.Return<TCauseIdentifier[], TRight>(), properties);
+            return Check(source.Return<TRight, TCauseIdentifier>(), properties);
         }
 
-        public static Either<string[], TRight> Check<TRight>(this TRight source,
+        public static Either<TRight, string> Check<TRight>(this TRight source,
             string cause,
             params Predicate<TRight>[] rules)
         {
-            return Check(source.Return<string[], TRight>(),
+            return Check(source.Return<TRight, string>(),
                 rules.Select(predicate => new Property<string, TRight>(cause, predicate)).ToArray());
         }
 
-        public static Either<string[], TRight> Check<TRight>(this TRight source,
+        public static Either<TRight, string> Check<TRight>(this TRight source,
             params Predicate<TRight>[] rules)
         {
-            return Check(source.Return<string[], TRight>(),
+            return Check(source.Return<TRight, string>(),
                 rules.Select(predicate => new Property<string, TRight>(Guid.NewGuid().ToString(), predicate)).ToArray());
         }
 
-        public static Either<string[], TRight> Check<TRight>(this Either<string[], TRight> source,
+        public static Either<TRight, string> Check<TRight>(this Either<TRight, string> source,
             string cause,
             params Predicate<TRight>[] rules)
         {
