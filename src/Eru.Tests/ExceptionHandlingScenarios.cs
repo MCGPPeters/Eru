1de6a05b7f06c7cb0c53cdfce4dd2c23e562420c
Maurice CGP Peters <mcgppeters@hotmail.com>
2017-05-19 09:03:04 +0200

    Added verfication that retries happen the expected ammount of times

--- src/Eru.Tests/ExceptionHandlingScenarios.cs
+++ src/Eru.Tests/ExceptionHandlingScenarios.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
-//using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using FsCheck;
 using FsCheck.Xunit;
@@ -12,79 +12,78 @@ using static Xunit.Assert;
 
 namespace Eru.Tests
 {
-    using static _;
-
     [SuppressMessage("ReSharper", "ConvertToConstant.Local",
         Justification = "Using a const integer as a divisor will cause a compile time error")]
     public class ExceptionHandlingProperties
     {
         private static void Fail() => True(false);
+
         private static void Succeed() => True(true);
 
         [Property(Verbose = true, DisplayName =
             "Dividing an integer by a non zero number returns the expected result")]
-        public void Property1(PositiveInt dividend, PositiveInt divisor) =>
-            dividend
+        public void Property1(PositiveInt dividend, PositiveInt divisor) => dividend
                 .Try(x => x.Get / divisor.Get)
                 .Match(
-                    i => Equal(dividend.Get / divisor.Get, i), 
+                    i => Equal(dividend.Get / divisor.Get, i),
                     _ => Fail());
 
         [Property(Verbose = true, DisplayName =
             "Dividing an integer by zero return an exception")]
-        public void Property2(int dividend) =>
-            dividend
+        public void Property2(int dividend) => dividend
                 .Try(x => x / 0)
                 .Match(
-                    _ => Fail(), 
+                    _ => Fail(),
                     exception => IsType(typeof(DivideByZeroException), exception));
 
         [Property(Verbose = true, DisplayName =
             "Chaining of try blocks is possible for disparately typed return values")]
-        public void Property3(PositiveInt dividend, PositiveInt divisor, PositiveInt secondDivisor) =>
-            dividend
+        public void Property3(PositiveInt dividend, PositiveInt divisor, PositiveInt secondDivisor) => dividend
                 .Try(x => x.Get / divisor.Get)
                 .Try(x => x / secondDivisor.Get)
                 .Match(
-                    i => Equal(dividend.Get / divisor.Get / secondDivisor.Get, i), 
+                    i => Equal(dividend.Get / divisor.Get / secondDivisor.Get, i),
                     _ => Fail());
 
         [Property(Verbose = true, DisplayName =
             "Retry should return the expected failure when the condition does not hold")]
-        public void Property4(PositiveInt dividend)
-            => dividend
+        public void Property4(PositiveInt dividend) => dividend
                 .Retry(x => x.Get / 0, TimeSpan.FromMilliseconds(20))
                 .Match(
                     _ => Fail(),
                     exception => IsType(typeof(DivideByZeroException), exception));
 
 
-
         [Property(Verbose = true, DisplayName =
             "Retry only as long as the call fails")]
-        public void Property5(PositiveInt numberOfCalls) =>
-            numberOfCalls
+        public void Property5(PositiveInt numberOfCalls) => numberOfCalls
                 .Get
-                .Retry(x => (x * 3) % 3 == 0
+                .Retry(x => x * 3 % 3 == 0
                     ? x
                     : throw new Exception())
                 .Match(i => Succeed(), exception => Fail());
 
+        [Property(Verbose = true, DisplayName =
+            "Retries equal the number of delays")]
+        public void Property6(PositiveInt numberOfCalls)
+        {
+            var numberOfTries = 0;
+            Task
+                .Run(() =>
+                {
+                    if (numberOfCalls.Get == numberOfTries) return;
+                    numberOfTries++;
+                    throw new Exception("foo");
+                })
+                .Retry(Enumerable.Repeat(TimeSpan.FromMilliseconds(1), numberOfCalls.Get).ToArray())
+                .Otherwise(() => Task.Run(() => Equal(numberOfCalls.Get - 1, numberOfTries))).Wait();
+        }
+
         [Fact(DisplayName =
             "Retry can run asynchrounously")]
-        public async Task Property6() =>
-            await new SqlConnection()
+        public async Task Property7() => await new SqlConnection()
                 .OpenAsync()
                 .Retry(TimeSpan.FromMilliseconds(20), TimeSpan.FromMilliseconds(20))
                 .Otherwise(exception => IsType(typeof(InvalidOperationException), exception));
-
-
-        //        .Otherwise(exception => throw exception).Wait();[Fact(DisplayName =
-        //    "Retry can run asynchrounously")]
-        //public void Property6() =>
-        //    new SqlConnection()
-        //        .OpenAsync()
-        //        .Retry(TimeSpan.FromMilliseconds(20), TimeSpan.FromMilliseconds(20))
-        //        .Otherwise(exception => throw exception).Wait();
     }
 }
