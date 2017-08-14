4e4fff87ab4f4313e26fd8a145839675d8076c83
Maurice CGP Peters <mcgppeters@hotmail.com>
2017-08-14 19:51:23 +0200

    Enumerable extensions added

--- src/Eru/EnumerableExtensions.cs
+++ src/Eru/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace Eru
@@ -17,6 +18,36 @@ namespace Eru
             // ReSharper disable once LoopCanBeConvertedToQuery => Does not work here. Will be ambiguous invocation
             foreach (var element in source) result = await func(result, element);
             return result;
+        }
+
+        /// <summary>
+        /// Partition the sequence in parts of <paramref name="length"/>. The operation is O(1)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>>
+            Partition<T>(this IEnumerable<T> source, int length)
+        {
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            var partition = new List<T>(length);
+
+            foreach (var item in source)
+            {
+                partition.Add(item);
+
+                if (partition.Count == length)
+                {
+                    yield return partition.AsReadOnly();
+                    partition = new List<T>(length);
+                }
+            }
+
+            if (partition.Count > 0)
+                yield return partition.AsReadOnly();
         }
     }
 }
