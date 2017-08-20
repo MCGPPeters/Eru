1f41b4517b13b10b9b381d53d332a1cf9a7a65ce
Maurice CGP Peters <mcgppeters@hotmail.com>
2017-08-20 17:14:50 +0200

    Curry helpers

---
+++ src/Eru/FunctionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eru
+{
+    public static partial class _
+    {
+        public static Func<T1, Func<T2, TResult>> Curry<T1, T2, TResult>(this Func<T1, T2, TResult> func)
+            => t1 => t2 => func(t1, t2);
+
+        public static Func<T1, Func<T2, Func<T3, TResult>>> Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func)
+            => t1 => t2 => t3 => func(t1, t2, t3);
+    }
+}
